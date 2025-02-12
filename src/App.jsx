f9687ea8c46fec3e953909632452aa5ed2bc726c
Muhammad Nouman <03035777248a@gmail.com>
2025-02-12 11:22:38 +0500

    change favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./Pages/Layout";
 import PrivacyPolicy from "./Pages/PrivacyPolicy";
